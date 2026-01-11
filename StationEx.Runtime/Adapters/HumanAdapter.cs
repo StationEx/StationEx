2ba723fc0d552ed323489b01985a60341889e671
Michael Gray <54971998+michaeljgray@users.noreply.github.com>
2026-01-11 01:17:53 -0800

    Added attribute to describe type adapters. Removed setters for HumanAdapter.

--- StationEx.Runtime/Adapters/HumanAdapter.cs
+++ StationEx.Runtime/Adapters/HumanAdapter.cs
@@ -2,41 +2,37 @@
 {
     using StationEx.Runtime.Integration;
 
+    [TypeAdapter("Assembly-CSharp.dll", "Assets.Scripts.Objects.Entities.Human")]
     internal sealed class HumanAdapter
     {
         [PropertyBinding("ReferenceId")]
         public long ReferenceId
         {
             get;
-            set;
         }
 
         [PropertyBinding("OwnerClientId")]
         public ulong OwnerClientId
         {
             get;
-            set;
         }
 
         [PropertyBinding("name")]
         public string Name
         {
             get;
-            set;
         }
 
         [FieldBinding("_oldState")]
         public EntityStateAdapter PreviousState
         {
             get;
-            set;
         }
 
         [PropertyBinding("State")]
         public EntityStateAdapter CurrentState
         {
             get;
-            set;
         }
     }
 }
---
+++ StationEx.Runtime/Integration/TypeAdapterAttribute.cs
@@ -0,0 +1,33 @@
+﻿namespace StationEx.Runtime.Integration
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    internal sealed class TypeAdapterAttribute : Attribute
+    {
+        private readonly string module;
+        private readonly string type;
+
+        public string Module
+        {
+            get
+            {
+                return this.module;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return this.type;
+            }
+        }
+
+        public TypeAdapterAttribute(string module, string type)
+        {
+            this.module = module;
+            this.type = type;
+        }
+    }
+}
