da6b6dcea040b5d89b83e6133831eadc8df436d0
Michael Gray <54971998+michaeljgray@users.noreply.github.com>
2026-01-11 12:23:41 -0800

    Extracted helpers for managing integrations and type adapters.

---
+++ StationEx/IntegrationHelper.cs
@@ -0,0 +1,96 @@
+﻿namespace StationEx
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using Mono.Cecil;
+
+    internal static class IntegrationHelper
+    {
+        private static bool IsIntegrationAttribute(CustomAttribute attribute)
+        {
+            return
+                attribute.AttributeType.Module.Name == "StationEx.Runtime.dll" &&
+                attribute.AttributeType.FullName == "StationEx.Runtime.Integration.IntegrationAttribute" &&
+                attribute.ConstructorArguments.Count == 4 &&
+                attribute.ConstructorArguments[0].Type.FullName == "StationEx.Runtime.Integration.IntegrationMode" &&
+                attribute.ConstructorArguments[1].Type.FullName == "System.String" &&
+                attribute.ConstructorArguments[2].Type.FullName == "System.String" &&
+                attribute.ConstructorArguments[3].Type.FullName == "System.String";
+        }
+
+        private static bool TryGetIntegrationDescription(MethodDefinition method, [NotNullWhen(true)] out IntegrationDescription? description)
+        {
+            if (method.IsStatic)
+            {
+                foreach (CustomAttribute attribute in method.CustomAttributes)
+                {
+                    if (IsIntegrationAttribute(attribute))
+                    {
+                        description = new IntegrationDescription(
+                            (int)attribute.ConstructorArguments[0].Value,
+                            (string)attribute.ConstructorArguments[1].Value,
+                            (string)attribute.ConstructorArguments[2].Value,
+                            (string)attribute.ConstructorArguments[3].Value);
+
+                        return true;
+                    }
+                }
+            }
+
+            description = null;
+            return false;
+        }
+
+        private static bool TryGetIntegration(MethodDefinition sourceMethod, IntegrationDescription description, AssemblyDefinition targetAssembly, [NotNullWhen(true)] out Integration? integration)
+        {
+            ModuleDefinition? targetModule = targetAssembly.Modules.SingleOrDefault(module => module.Name == description.TargetModuleName);
+            if (targetModule is null)
+            {
+                integration = null;
+                return false;
+            }
+
+            TypeDefinition? targetType = targetModule.Types.SingleOrDefault(type => type.FullName == description.TargetTypeName);
+            if (targetType is null)
+            {
+                integration = null;
+                return false;
+            }
+
+            MethodDefinition? targetMethod = targetType.Methods.SingleOrDefault(method => method.FullName == description.TargetMethodName);
+            if (targetMethod is null)
+            {
+                integration = null;
+                return false;
+            }
+
+            integration = new Integration((IntegrationType)description.Mode, sourceMethod, targetMethod);
+            return true;
+        }
+
+        public static List<Integration> GetIntegrations(AssemblyDefinition assembly, AssemblyDefinition target)
+        {
+            List<Integration> integrations = new List<Integration>();
+
+            foreach (ModuleDefinition sourceModule in assembly.Modules)
+            {
+                foreach (TypeDefinition sourceType in sourceModule.Types)
+                {
+                    foreach (MethodDefinition sourceMethod in sourceType.Methods)
+                    {
+                        if (TryGetIntegrationDescription(sourceMethod, out IntegrationDescription? description))
+                        {
+                            if (TryGetIntegration(sourceMethod, description, target, out Integration? integration))
+                            {
+                                integrations.Add(integration);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return integrations;
+        }
+    }
+}
---
+++ StationEx/TypeAdapterHelper.cs
@@ -0,0 +1,83 @@
+﻿namespace StationEx
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using Mono.Cecil;
+
+    internal static class TypeAdapterHelper
+    {
+        private static bool IsTypeAdapterAttribute(CustomAttribute attribute)
+        {
+            return
+                attribute.AttributeType.Module.Name == "StationEx.Runtime.dll" &&
+                attribute.AttributeType.FullName == "StationEx.Runtime.Integration.TypeAdapterAttribute" &&
+                attribute.ConstructorArguments.Count == 2 &&
+                attribute.ConstructorArguments[0].Type.FullName == "System.String" &&
+                attribute.ConstructorArguments[1].Type.FullName == "System.String";
+        }
+
+        private static bool TryGetTypeAdapterDescription(TypeDefinition type, [NotNullWhen(true)] out TypeAdapterDescription? description)
+        {
+            if (type.IsClass || type.IsValueType)
+            {
+                foreach (CustomAttribute attribute in type.CustomAttributes)
+                {
+                    if (IsTypeAdapterAttribute(attribute))
+                    {
+                        description = new TypeAdapterDescription(
+                            (string)attribute.ConstructorArguments[0].Value,
+                            (string)attribute.ConstructorArguments[1].Value);
+
+                        return true;
+                    }
+                }
+            }
+
+            description = null;
+            return false;
+        }
+
+        private static bool TryGetTypeAdapter(TypeDefinition sourceType, TypeAdapterDescription description, AssemblyDefinition targetAssembly, [NotNullWhen(true)] out TypeAdapter? adapter)
+        {
+            ModuleDefinition? targetModule = targetAssembly.Modules.SingleOrDefault(module => module.Name == description.TargetModuleName);
+            if (targetModule is null)
+            {
+                adapter = null;
+                return false;
+            }
+
+            TypeDefinition? targetType = targetModule.Types.SingleOrDefault(type => type.FullName == description.TargetTypeName);
+            if (targetType is null)
+            {
+                adapter = null;
+                return false;
+            }
+
+            adapter = new TypeAdapter(sourceType, targetType);
+            return true;
+        }
+
+        public static List<TypeAdapter> GetTypeAdapters(IEnumerable<Integration> integrations, AssemblyDefinition target)
+        {
+            List<TypeAdapter> adapters = new List<TypeAdapter>();
+
+            foreach (Integration integration in integrations)
+            {
+                foreach (ParameterDefinition sourceParameter in integration.Source.Parameters)
+                {
+                    TypeDefinition sourceType = sourceParameter.ParameterType.Resolve();
+                    if (TryGetTypeAdapterDescription(sourceType, out TypeAdapterDescription? description))
+                    {
+                        if (TryGetTypeAdapter(sourceType, description, target, out TypeAdapter? adapter))
+                        {
+                            adapters.Add(adapter);
+                        }
+                    }
+                }
+            }
+
+            return adapters;
+        }
+    }
+}
