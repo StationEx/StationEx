8901b78d466874676b42e11d745ec65eb1f6970e
Michael Gray <54971998+michaeljgray@users.noreply.github.com>
2026-01-11 12:25:39 -0800

    Small refactor around the new constants and helpers.

--- StationEx/AssemblyPatch.cs
+++ StationEx/AssemblyPatch.cs
@@ -2,113 +2,33 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics.CodeAnalysis;
     using System.Linq;
-    using System.Text;
     using Mono.Cecil;
-    using Mono.Cecil.Rocks;
-    using Mono.CompilerServices.SymbolWriter;
+    using StationEx.Constants;
 
     internal static class AssemblyPatch
     {
-        private static bool IsValidIntegrationAttribute(CustomAttribute attribute)
-        {
-            return
-                attribute.AttributeType.Module.Name == "StationEx.Runtime.dll" &&
-                attribute.AttributeType.FullName == "StationEx.Runtime.Integration.IntegrationAttribute" &&
-                attribute.ConstructorArguments.Count == 4 &&
-                attribute.ConstructorArguments[0].Type.FullName == "StationEx.Runtime.Integration.IntegrationMode" &&
-                attribute.ConstructorArguments[1].Type.FullName == "System.String" &&
-                attribute.ConstructorArguments[2].Type.FullName == "System.String" &&
-                attribute.ConstructorArguments[3].Type.FullName == "System.String";
-        }
-
-        private static bool TryGetIntegrationDescription(MethodDefinition method, [NotNullWhen(true)] out IntegrationDescription? description)
-        {
-            if (method.IsStatic)
-            {
-                foreach (CustomAttribute attribute in method.CustomAttributes)
-                {
-                    if (IsValidIntegrationAttribute(attribute))
-                    {
-                        description = new IntegrationDescription(
-                            (int)attribute.ConstructorArguments[0].Value,
-                            (string)attribute.ConstructorArguments[1].Value,
-                            (string)attribute.ConstructorArguments[2].Value,
-                            (string)attribute.ConstructorArguments[3].Value);
-
-                        return true;
-                    }
-                }
-            }
-
-            description = null;
-            return false;
-        }
-
-        private static bool TryGetIntegration(MethodDefinition sourceMethod, IntegrationDescription description, AssemblyDefinition targetAssembly, [NotNullWhen(true)] out Integration? integration)
-        {
-            ModuleDefinition? targetModule = targetAssembly.Modules.SingleOrDefault(module => module.Name == description.TargetModuleName);
-            if (targetModule is null)
-            {
-                integration = null;
-                return false;
-            }
-
-            TypeDefinition? targetType = targetModule.Types.SingleOrDefault(type => type.FullName == description.TargetTypeName);
-            if (targetType is null)
-            {
-                integration = null;
-                return false;
-            }
-
-            MethodDefinition? targetMethod = targetType.Methods.SingleOrDefault(method => method.FullName == description.TargetMethodName);
-            if (targetMethod is null)
-            {
-                integration = null;
-                return false;
-            }
-
-            integration = new Integration((IntegrationType)description.Mode, sourceMethod, targetMethod);
-            return true;
-        }
-
-        private static List<Integration> GetIntegrations(AssemblyDefinition assembly, AssemblyDefinition target)
-        {
-            List<Integration> integrations = new List<Integration>();
-
-            foreach (ModuleDefinition sourceModule in assembly.Modules)
-            {
-                foreach (TypeDefinition sourceType in sourceModule.Types)
-                {
-                    foreach (MethodDefinition sourceMethod in sourceType.Methods)
-                    {
-                        if (TryGetIntegrationDescription(sourceMethod, out IntegrationDescription? description))
-                        {
-                            if (TryGetIntegration(sourceMethod, description, target, out Integration? integration))
-                            {
-                                integrations.Add(integration);
-                            }
-                        }
-                    }
-                }
-            }
-
-            return integrations;
-        }
-
         private static void DeleteRuntimeReference(AssemblyDefinition target)
         {
-            AssemblyNameReference? stationExRuntimeReference = target.MainModule.AssemblyReferences.SingleOrDefault(reference => reference.Name == "StationEx.Runtime");
+            AssemblyNameReference? stationExRuntimeReference = target.MainModule.AssemblyReferences.SingleOrDefault(reference => reference.Name == AssemblyNames.StationExRuntime);
             if (stationExRuntimeReference is not null)
             {
                 target.MainModule.AssemblyReferences.Remove(stationExRuntimeReference);
             }
         }
 
+        private static void DeleteStaticTypeAdapters(AssemblyDefinition target)
+        {
+            TypeDefinition? integrationAdapterType = target.MainModule.Types.SingleOrDefault(type => type.FullName == TypeNames.StationExTypeAdapterFull);
+            if (integrationAdapterType is not null)
+            {
+                target.MainModule.Types.Remove(integrationAdapterType);
+            }
+        }
+
         private static void DeleteStaticIntegrations(AssemblyDefinition target)
         {
-            TypeDefinition? integrationCoreType = target.MainModule.Types.SingleOrDefault(type => type.FullName == "StationEx.Generated.IntegrationCore");
+            TypeDefinition? integrationCoreType = target.MainModule.Types.SingleOrDefault(type => type.FullName == TypeNames.StationExIntegrationCoreFull);
             if (integrationCoreType is not null)
             {
                 target.MainModule.Types.Remove(integrationCoreType);
@@ -117,18 +37,27 @@
 
         private static void ApplyRuntimeReference(AssemblyDefinition target)
         {
-            AssemblyNameReference reference = new AssemblyNameReference("StationEx.Runtime", new Version("1.0.0.0"));
+            AssemblyNameReference reference = new AssemblyNameReference(AssemblyNames.StationExRuntime, Versions.CurrentStationExRuntime);
             target.MainModule.AssemblyReferences.Add(reference);
+        }
+
+        private static void CopyStaticTypeAdapters(AssemblyDefinition target, IEnumerable<TypeAdapter> adapters)
+        {
+            TypeDefinition integrationCoreType = new TypeDefinition(Namespaces.StationExGenerated, TypeNames.StationExTypeAdapter, TypeAttributes.NotPublic | TypeAttributes.Abstract | TypeAttributes.Sealed);
+            foreach (TypeAdapter adapter in adapters)
+            {
+                throw new NotImplementedException();
+            }
         }
 
         private static void CopyStaticIntegrationHandlers(AssemblyDefinition target, IEnumerable<Integration> integrations)
         {
-            TypeDefinition integrationCoreType = new TypeDefinition("StationEx.Generated", "IntegrationCore", TypeAttributes.NotPublic | TypeAttributes.Abstract | TypeAttributes.Sealed);
+            TypeDefinition integrationCoreType = new TypeDefinition(Namespaces.StationExGenerated, TypeNames.StationExIntegrationCore, TypeAttributes.NotPublic | TypeAttributes.Abstract | TypeAttributes.Sealed);
 
             foreach (Integration sourceIntegration in integrations)
             {
                 MethodDefinition targetHandler = new MethodDefinition(
-                    $"StationEx.Generated.{sourceIntegration.Source.Name}",
+                    $"{Namespaces.StationExGenerated}.{sourceIntegration.Source.Name}",
                     MethodAttributes.Public | MethodAttributes.Static | MethodAttributes.HideBySig,
                     sourceIntegration.Source.ReturnType);
 
@@ -151,12 +80,17 @@
 
         private static void ApplyStaticIntegrations(AssemblyDefinition source, AssemblyDefinition target, Integration integration)
         {
-
+            throw new NotImplementedException();
         }
 
         private static void ApplyStaticIntegrations(AssemblyDefinition source, AssemblyDefinition target)
         {
-            List<Integration> integrations = GetIntegrations(source, target);
+            DeleteStaticTypeAdapters(target);
+
+            List<Integration> integrations = IntegrationHelper.GetIntegrations(source, target);
+            List<TypeAdapter> adapters = TypeAdapterHelper.GetTypeAdapters(integrations, target);
+
+            CopyStaticTypeAdapters(source, adapters);
             CopyStaticIntegrationHandlers(target, integrations);
 
             foreach (Integration integration in integrations)
@@ -167,11 +101,14 @@
 
         public static void ApplyRuntimeIntegration(AssemblyDefinition source, AssemblyDefinition target)
         {
-            DeleteStaticIntegrations(target);
-            DeleteRuntimeReference(target);
-
             ApplyRuntimeReference(target);
             ApplyStaticIntegrations(source, target);
+        }
+
+        public static void DeleteRuntimeIntegration(AssemblyDefinition target)
+        {
+            DeleteStaticIntegrations(target);
+            DeleteRuntimeReference(target);
         }
     }
 }
